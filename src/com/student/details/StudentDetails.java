a2827f66c17aed32df44bbc2419a687913d6596a
PraveenFelicity <praveensundararajan17@gmail.com>
2021-06-16 17:31:21 +0530

    Scanner Task

---
+++ src/com/student/details/StudentDetails.java
@@ -0,0 +1,24 @@
+package com.student.details;
+
+import java.util.Scanner;
+
+public class StudentDetails {
+ public static void main (String [] args) {
+	 Scanner sc = new Scanner (System.in);
+	 int i = sc.nextInt();
+	 System.out.println("StudId:"+ i);
+	 String st =sc.next();
+	 System.out.println("StudName:"+ st);
+	 int i1 = sc.nextInt();
+	 System.out.println("M1:"+i1);
+	 int i2 =sc.nextInt();
+	 System.out.println("M2:"+i2);
+	 int i3 = sc.nextInt();
+	 System.out.println("M3:"+i3);
+	 int i4 = sc.nextInt();
+	 System.out.println("M4:"+i4);
+	 int i5 = sc.nextInt();
+	 System.out.println("M5:"+i5);
+ }
+ 
+}
